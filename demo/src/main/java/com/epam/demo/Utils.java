39f86e569478c70046740f81e93b503f8275a8c1
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-03 01:46:17 +0300

    Added solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,22 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if (args.contains("null")) {
+            return false;
+        }
+        boolean positiveCheck = true;
+        for (String arg : args) {
+            if (arg.isEmpty() || !StringUtils.isNotBlank(arg) || Double.parseDouble(arg) <= 0) {
+                return false;
+            }
+        }
+        return positiveCheck;
+        //magic happens here
         //magic happens here
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        return StringUtils.isPositiveNumber(str);
          //here magic will happen
     }
 }
